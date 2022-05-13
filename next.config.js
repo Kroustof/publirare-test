f2583b2040313a415de5772c4d1cd2439862403e
Kroustof <didier.billars@hotmail.fr>
2022-05-13 09:16:28 +0400

    all page added + del variables in config

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
-  // env: {
-  //   "MORALIS_SERVER_URL":"https://d5cvkzmvd0lt.usemoralis.com:2053/server",
-  //   "MORALIS_APP_ID":"B9dYSr4Q9oj8DQah7jFqIz4uyhrXohKJQbirpzsU",
-  //   "WEB3AUTH_CLIENT_ID":"BIeaJTx34Mb-zQCm1mxUNpbGUEI14xuHRQZt4hZnn_Q9JwEdhxBxSqxqGIcTDAF8VwreWOzldQhBLCR7BOfn7tc"
-  // },
   i18n,
   webpack: (config) => {
     config.resolve.fallback = { 
